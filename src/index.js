a1952dca31f40d6945504b6b50e9abf008ceaf34
Raquel Maia <raquelmaia860@gmail.com>
2023-04-29 16:48:25 -0300

    Projeto Card validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,77 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+// Seleciona os elementos HTML por meio do ID
+const validNumber = document.getElementById("validNumber");
+const button = document.getElementById("validateButton");
+const message = document.getElementById("message");
+
+
+// Adiciona um listener de evento ao botão para disparar a função "number" quando clicado
+button.addEventListener('click', number);
+
+
+// Função "number" que será disparada quando o botão for clicado
+function number(evento) {
+  // Previne o comportamento padrão do formulário de ser enviado
+  evento.preventDefault();
+ 
+  // Obtém o valor do número de cartão inserido pelo usuário
+  const numeroCartao = validNumber.value;
+ 
+  // Verifica se o número de cartão é válido usando a função "isValid" do módulo "validator"
+  const validador = validator.isValid(numeroCartao);
+
+
+  // Se o número de cartão for menor que 16, exibe a mensagem de "Número de Cartão Incompleto"
+  if (numeroCartao.length < 16) {
+    return (message.innerHTML = "Número de Cartão Incompleto");
+  }
+  // Se o número de cartão for válido, exibe a mensagem de "Seu Cartão é válido" com a máscara aplicada ao número de cartão
+  else if (validador === true) {
+    return (message.innerHTML = `Seu Cartão é válido ${validator.maskify(numeroCartao)}`);
+  }
+  // Se o número de cartão for inválido, exibe um alerta com a mensagem "Cartão Inválido"
+  else if (!validador) {
+    alert("Cartão Inválido");
+  }
+}
+
+
+// Seleciona o elemento HTML para exibir o número de cartão mascarado
+const validNumberTwo = document.getElementById("validNumberTwo");
+
+
+// Adiciona um listener de evento que será disparado toda vez que o usuário digitar algo no campo do número de cartão
+validNumber.addEventListener("input", function() {
+  // Obtém o valor do número de cartão digitado
+  const originalValue = validNumber.value;
+   
+  // Remove todos os caracteres não numéricos do número de cartão
+  const numericValue = originalValue.replace(/\D/g, '');
+   
+  // Exibe o valor do número de cartão sem máscara no segundo campo
+  validNumberTwo.value = numericValue;
+});
+ 
+
+
+// Seleciona os elementos HTML para os campos de nome do usuário e exibe o valor digitado no segundo campo
+const nameTextOne = document.getElementById("nameTextOne");
+const nameTextTwo = document.getElementById("nameTextTwo");
+
+
+nameTextOne.addEventListener("input", function() {
+  nameTextTwo.value = nameTextOne.value;
+});
+
+
+// Seleciona os elementos HTML para os campos de senha do usuário e exibe o valor digitado no segundo campo
+const textPassword = document.getElementById("textPassword");
+const textPassword2 = document.getElementById("textPassword2");
+
+
+textPassword.addEventListener("input", function() {
+  textPassword2.value = textPassword.value;
+});
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,55 @@
 const validator = {
-  // ...
-};
+  isValid(creditCardNumber) {
+    // define duas variáveis, uma para armazenar a soma dos dígitos do número e outra para verificar se o próximo dígito deve ser duplicado
+    let sum = 0;
+    let double = false;
 
+
+    // percorre cada dígito do número de cartão de crédito, do último para o primeiro
+    for (let i = creditCardNumber.length - 1; i >= 0; i--) {
+      // obtém o dígito atual
+      let digit = parseInt(creditCardNumber.charAt(i));
+
+
+      // se o dígito deve ser duplicado, o duplica e subtrai 9 se o resultado for maior que 9
+      if (double) {
+        digit *= 2;
+        if (digit > 9) {
+          digit -= 9;
+        }
+      }
+
+
+      // adiciona o dígito atual à soma e alterna a flag de duplicação
+      sum += digit;
+      double = !double;
+    }
+
+
+    // retorna true se a soma é divisível por 10, o que significa que o número é válido, caso contrário retorna false
+    return (sum % 10) === 0;
+  },
+
+
+  // esta é uma função que mascara os dígitos do número de cartão de crédito, mantendo apenas os últimos 4 dígitos
+  maskify(creditCardNumber) {
+    // se o número de cartão de crédito tem 4 dígitos ou menos, retorna o próprio número
+    if (creditCardNumber.length <= 4) {
+      return creditCardNumber;
+    }
+   
+    // obtém os últimos 4 dígitos do número de cartão de crédito
+    const lastFourDigits = creditCardNumber.slice(-4);
+    // cria uma string com o caractere "#" repetido várias vezes para o número de dígitos menos os últimos 4
+    const maskedDigits = "#".repeat(creditCardNumber.length - 4);
+   
+    // retorna a string com os dígitos mascarados mais os últimos 4 dígitos do número de cartão de crédito
+    return maskedDigits + lastFourDigits;
+  }
+};
 export default validator;
+
+
+
+
+
